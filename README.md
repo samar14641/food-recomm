4da41612da5a4b119c6b2b0e265f88b9fa3fcda8
Samar Dikshit <samard05@gmail.com>
2020-07-24 23:04:31 -0400

    added cleaned_ingredients.txt and preprocess code

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # food-recomm
 
+[Dataset](http://www.ub.edu/cvub/recipes5k/)
+
 /Data/images/ is in .gitignore! 
+
+Cleaned data in /Data/annotations/cleaned_ingredients.txt
+
+See unicode.txt for unicode replacements
---
+++ preprocess.py
@@ -0,0 +1,32 @@
+import os
+# import unicodedata
+
+
+filePath = os.getcwd() + '/Data/annotations/ingredients_simplified_Recipes5k.txt'
+toRemove = set(['& half', '& half\n'])  # list of ingredients to remove
+
+def clean():
+    cleaned = []
+
+    with open(filePath) as dataset:
+        line = dataset.readline().lower().strip()
+        
+        while line:
+            # line = unicodedata.normalize('NFKD', line).encode('ascii', 'ignore').decode('utf-8')  # this didn't work
+
+            ingredients = ','.join(list(set([i.strip() for i in line.split(',') if i not in toRemove])))
+            if '\\u' in ingredients:
+                ingredients = ingredients.replace('\\u00ae', '').replace('\\u00e2', 'a').replace('\\u00e8', 'e').replace('\\u00e9', 'e').replace('\\u00ee', 'i').replace('\\u00fa', 'u').replace('\\u00fc', 'u').replace('\\u2122', '')
+                
+            cleaned.append(ingredients)
+
+            line = dataset.readline()
+
+    opFile = open(os.getcwd() + '/Data/annotations/cleaned_ingredients.txt', 'w+')
+
+    for i in cleaned:
+        opFile.write(i + '\n')
+
+    opFile.close()
+
+clean()
